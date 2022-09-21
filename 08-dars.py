a40f00ec7e609f785754e4089869c03ed957f63a
samuraixan <samuraikol@mail.ru>
2022-09-21 09:15:22 +0500

    Yangi fayllar qo'shildi
    7-8 darslar yuklandi

---
+++ 08-dars.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 11:05:58 2022
+
+@author: Kamolxon
+"""
+
+# cars=['bmw','mersedes benz','volvo','general motors','tesla','audi']
+# cars.sort()
+# print(cars)
+# cars.sort(reverse=True)
+# print(cars)
+
+# mehmonlar=['Odil','Hamid','Temur','Avazbek','Farruh','Shamsiddin']
+# print(sorted(mehmonlar))
+# print(mehmonlar)
+# print(sorted(mehmonlar,reverse=True))
+
+# ages=[12,98,34,65,34,76,11]
+# ages.sort()
+# print(ages)
+# print(sorted(ages,reverse=True))
+
+# mevalar=['olma','banan','limon','anor','uzum']
+# mevalar.reverse()
+# print(mevalar)
+# print(len(mevalar))
+
+# sonlar=list(range(0,10))
+# print(sonlar)
+
+# juft_sonlar=list(range(0,20,2))
+# toq_sonlar=list(range(1,20,2))
+# print(juft_sonlar)
+# print(toq_sonlar)
+
+# narhlar=[12000,22500,23456,9800,5600,9934,32874]
+# arzon=min(narhlar)
+# qimmat=max(narhlar)
+# jami=sum(narhlar)
+# print("Eng arzon narh", arzon,"Eng qimmati",qimmat,"Jami:",jami)
+
+# cars=['bmw','mersedes benz','volvo','general motors','tesla','audi']
+# my_cars=cars[0:3]
+# print(my_cars)
+# print(cars[2:5])
+# print(cars[:4])
+# print(cars[2:])
+
+# sonlar=[1,2,3,4,5]
+# sonlar2=sonlar
+# sonlar2.append(6)
+# sonlar2.append(7)
+# print("Bu sonlar ro'yxati:",sonlar)
+# print("Bu sonlar ro'yxati",sonlar2)
+
+# sonlar=[1,2,3,4,5]
+# sonlar2=sonlar[:]
+# sonlar2.append(6)
+# sonlar2.append(7)
+# print("Bu sonlar ro'yxati:",sonlar)
+# print("Bu sonlar ro'yxati",sonlar2)
+
+# tomonlar=(20,30,55.2)
+# print(tomonlar)
+
+# toys=('bus','car','bear','dino','snake','lizard')
+# print(toys[0])
+# print(toys[-1])
+# print(toys[2:5])
+
+# toys=('bus','car','bear','dino','snake','lizard')
+# toys=list(toys)
+# toys.append('dragon')
+# toys.remove('bus')
+# toys[1]='mcqueen'
+# toys=tuple(toys)
+# print(toys)
---
+++ 7-dars/07-dars.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# mevalar=['olma','anjir','shaftoli',"o'rik"] 
+# mevalar.append("tarvuz")
+# print(mevalar)
+
+# narhlar=[12000,18000,10900,22000] 
+# sonlar=['bir','ikki',3,4,5]
+# ismlar=[]
+# print(mevalar)
+# print("Birinchi meva:",mevalar[0])
+# print("Ikkinchi meva:",mevalar[1])
+# print("Birinchi meva:",mevalar[0].title())
+# print("Ikkinchi meva:",mevalar[1].upper())
+
+# print(narhlar)
+# print(narhlar[2] + narhlar[3])
+
+# car_models=['Toyota','GM','Volvo','BMW','Hyundai','Kia','Volkswagen']
+# print(car_models[-1])
+
+# narhlar[0]=13000
+# narhlar[2]=11000
+# narhlar[3]=narhlar[3]+2000
+# print(narhlar)
+
+# cars=[]
+# cars.append('Lacetti')
+# cars.append('Nexia 3')
+# cars.append('Cobalt')
+# print(cars)
+# cars.insert(0,'Malibu')
+# print(cars)
+# cars.insert(2,'Damas')
+# print(cars)
+
+# mevalar=['olma','anjir','shaftoli',"o'rik",'anor']
+# del mevalar[1]
+# print(mevalar)
+# mevalar.remove('shaftoli')
+# print(mevalar)
+
+# bozorlik=["yog'",'un','piyoz','banan',"go'sht"]
+# mahsulot=bozorlik.pop(3)
+# print("Men " + mahsulot + " sotib oldim")
+# print("Olinmagan mahsulotlar:",bozorlik)
